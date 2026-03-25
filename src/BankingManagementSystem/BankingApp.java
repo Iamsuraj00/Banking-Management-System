c2bbb2b9eeec3c31bfff9ac951c3c1bdaa42194d
Suraj Sharma <112156492+Iamsuraj00@users.noreply.github.com>
2026-03-25 09:29:38 +0530

    make some database port changes

--- src/BankingManagementSystem/BankingApp.java
+++ src/BankingManagementSystem/BankingApp.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class BankingApp {
     private static final String url = "jdbc:mysql://localhost:3306/banking_system";
     private static final String username = "root";
-    private static final String password = "Narendra@123";
+    private static final String password = "root";
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         try{
